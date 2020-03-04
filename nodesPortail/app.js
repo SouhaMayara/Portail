3b540d317a989b7504bfce064f5d34dd3154d7d6
ghada <ghadabensalem0@gmail.com>
2020-03-04 14:57:26 +0100

    config node

--- nodesPortail/app.js
+++ nodesPortail/app.js
@@ -3,3 +3,11 @@ const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 app.use(bodyparser.json());
+
+mongoose.connect('mongodb://localhost:27017/absenceModule');
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  }); 
