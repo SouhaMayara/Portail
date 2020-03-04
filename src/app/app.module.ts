2ea572b7199697f02e992fabae7f18b02d5c9a9d
ghada <ghadabensalem0@gmail.com>
2020-03-04 12:58:54 +0100

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { ProfComponent } from './prof/prof.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EtudiantComponent,
+    ProfComponent
   ],
   imports: [
     BrowserModule
