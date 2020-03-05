2c5e45765101be16d3401dad3d58666e46a9f8a8
ghada <ghadabensalem0@gmail.com>
2020-03-05 21:33:34 +0100

    login api

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+app.use(bodyparser.json());
+
+mongoose.connect('mongodb://localhost:27017/absencedb')
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+const auth = require('./node/routes/auth')
+app.use('/auth', auth);
+
+app.listen(3001, ()=>{
+  console.log('port:3001')
+})
