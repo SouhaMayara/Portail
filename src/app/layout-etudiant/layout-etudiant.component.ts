41760e4efd4baf81dd117d249b16b9aa5216bd7b
SouhaMayara <mayara.souha@gmail.com>
2020-03-05 23:09:08 +0100

    RouteEtudiant

---
+++ src/app/layout-etudiant/layout-etudiant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-etudiant',
+  templateUrl: './layout-etudiant.component.html',
+  styleUrls: ['./layout-etudiant.component.css']
+})
+export class LayoutEtudiantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
