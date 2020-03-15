5eb901b26c2dbf067c679fe07a07ecb447f579d6
SouhaMayara <mayara.souha@gmail.com>
2020-03-15 12:07:22 +0100

    loginUser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import {HttpClient, HttpClientModule} from'@angular/common/http';
-
-
 import { AppComponent } from './app.component';
 
 
 import { LoginComponent } from './login/login.component';
 import { EtudiantComponent } from './etudiant/etudiant.component';
 import { ProfComponent } from './prof/prof.component';
-import { LayoutProfComponent } from './layout-prof/layout-prof.component';
-import { LayoutEtudiantComponent } from './layout-etudiant/layout-etudiant.component';
 import { AuthService } from './auth.service';
-
 import { ListePresenceComponent } from './liste-presence/liste-presence.component';
 
 @NgModule({
