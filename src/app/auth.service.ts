9783da261024b1a5c6f9f51ab17a25c4cc3c476c
SouhaMayara <mayara.souha@gmail.com>
2020-03-16 17:37:41 +0100

    userName

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,31 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-//import * as jwt_decode from 'jwt_decode';
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   userId;
-  //private _loginUrl="http://localhost:3001/auth/login";
+  
   constructor(private http: HttpClient) { }
 
-  /*loginUser(user){
-    return this.http.post<any>(this._loginUrl,user)
-  }
-
-  loggedIn() {
-    return !!localStorage.getItem('token')
-    }
-  }*/
   login(form){
     return this.http.post('http://localhost:3001/auth/login',form);
+  }
+
+  getUser() {
+    return this.http.get('http://localhost:3001/user/byUser/'+this.userId);
   }
 
   decodeToken() {
     if (localStorage.getItem('token')) {
       const token = localStorage.getItem('token');
-      //this.userId = jwt_decode(token).data._id;
+      console.log(jwt_decode(token));
+      this.userId = jwt_decode(token).data._id;
+      console.log(this.userId);
     }
   }
 
