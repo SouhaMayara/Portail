38f6258042226d3fd1fc562407bfa241660136d4
unknown <messoudamira1@gmail.com>
2020-04-08 16:50:21 +0200

    Updates_Models

--- node/models/absence.js
+++ node/models/absence.js
@@ -7,6 +7,7 @@ const absence = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
   matiere:{ type: mongoose.Schema.Types.ObjectId, ref: 'matiere' },
   seance: { type: mongoose.Schema.Types.ObjectId, ref: 'seance' },
+  justification : String,
 })
 
 module.exports = mongoose.model('absence', absence);
