b9736d00ab940a383a10e5f372c72f03d7314648
ghadabs <ghadabensalem0@gmail.com>
2020-04-10 12:48:19 +0100

    api getProf byUserId

--- node/routes/userId.js
+++ node/routes/userId.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
 const user = require('../models/user');
+const professeur = require('../models/professeur');
 
+router.get('/Prof/:idUser', async (req, res) => {
+  const profResult = await professeur.findOne({ "user": req.params.idUser }).exec();
+  res.send({ data: profResult })
+})
 
 router.get('/byUser/:id', async (req, res) => {
   const userResult = await user.findOne({ "_id": req.params.id }).exec();
   res.send({ data: userResult })
 })
+
+
 
 router.post('/updateProfile/:id', async (req, res) => {
     const userResult = await user.update({ "_id": req.params.id }, { $set: req.body }).exec();
