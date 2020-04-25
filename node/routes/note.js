194b7e85c67cd4a5cd0b87d9612a7e12f048a132
ghadabs <ghadabensalem0@gmail.com>
2020-04-25 14:52:03 +0100

    fix delete  and delete edit note api

--- node/routes/note.js
+++ node/routes/note.js
@@ -25,6 +25,18 @@ router.get('/:idS/:idm', async (req, res) => {
     res.send({ data: noteResult })
   })
 
+  router.post('/edit/:id',async (req, res) => {
+   
+    const noteResult = await note.updateOne({ "_id": req.params.id },{ $set: req.body }).exec();
+    res.send({ data: noteResult })
+  })
+
+  router.post('/delete/:id',async (req, res) => {
+   
+    const noteResult = await note.deleteOne({ "_id": req.params.id }).exec();
+    res.send({ data: noteResult })
+  })
+
 
 
 module.exports = router;
